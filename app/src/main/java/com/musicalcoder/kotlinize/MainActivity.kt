5662bcbf99ca2e70490fa8587207633258e4af81
Kelyn Paul Njeri <kelynpaul20.kp@gmail.com>
2019-03-29 23:35:42 +0300

    Initialize App Project.

---
+++ app/src/main/java/com/musicalcoder/kotlinize/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.musicalcoder.kotlinize
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
